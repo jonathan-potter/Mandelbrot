dbffc552519676056262bfd9d829d0479548a160
Jonathan Potter <jonathan.potter.business@gmail.com>
2014-08-25 23:43:34 -0700

    colors layers by iteration, adds click to zoom

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,80 +1,119 @@
 (function (window) {
-	var canvas, ctx, mandelbrot, pixels, x, y, z;
+	var draw_mandelbrot, pixels, x_range, y_range;
 
-	x_range = [-4.0, 4.0];
-	y_range = [-4.0, 4.0];
+	x_range = [-2.0, 2.0];
+	y_range = [-2.0, 2.0];
+	pixels = 400;
 
-	range_x = x_range[1] - x_range[0];
-	range_y = y_range[1] - y_range[0];
+	full_mandelbrot = function () {
 
-	pixels = 800;
-	delta_x = range_x / pixels;
-	delta_y = range_y / pixels;
+		range_x = x_range[1] - x_range[0];
+		range_y = y_range[1] - y_range[0];
+		delta_x = range_x / pixels;
+		delta_y = range_y / pixels;
 
-	iterations = 22;
+		iterations = 55;
 
-	mandelbrot = function (z) {
-		var c, i, r;
+		mandelbrot = function (z, iterations) {
+			var c, i, r, o;
 
-		c = z.c;
-		v = z.v;
-		r = v.r * v.r - v.i * v.i + c.r;
-		i = 2 * v.r * v.i + c.i;
+			c = z.c;
+			v = z.v;
+			r = v.r * v.r - v.i * v.i + c.r;
+			i = 2 * v.r * v.i + c.i;
+			o = z.o;
 
-		return {c: c, v: {r: r, i: i}};
-	};
+			if (Math.abs(r * r + i * i) > 4) {
+				o = o || iterations;
+			}
 
-	colormap = function (z) {
-		var r, g, b;
+			return {c: c, v: {r: r, i: i}, o: o};
+		};
 
-		z = Math.abs(z.v.r * z.v.r + z.v.i * z.v.i)
-		if (z === z) {
-			r = Math.round(z);
-		} else {
-			r = 255;
-		}
+		colormap = function (z) {
+			var r, g, b;
 
-		return r;
-	};
+			// z = Math.abs(z.v.r * z.v.r + z.v.i * z.v.i);
+			// if (z === z) {
+			// 	r = Math.round(z);
+			// } else {
+			// 	r = 0;
+			// }
+			if (z.o) {
+				r = 255 - (Math.round(z.o) * 8);
+			} else {
+				r = 0;
+			}
 
+			return r;
+		};
 
-	matrix = [];
-	for (var x_index = 0; x_index < pixels; x_index++) {
-		matrix.push([]);
-		for (var y_index = 0; y_index < pixels; y_index++) {
-			x = x_range[0] + x_index * delta_x;
-			y = y_range[0] + y_index * delta_y;
-
-			matrix[x_index][y_index] = {c: {r: x, i: y}, v: {r: 0, i: 0}};
-		}
-	}
-
-	/** iteration **/
-	for (i = 0; i < iterations - 1; i++) {
+		// initial setup
+		matrix = [];
 		for (var x_index = 0; x_index < pixels; x_index++) {
+			matrix.push([]);
 			for (var y_index = 0; y_index < pixels; y_index++) {
-				z = matrix[x_index][y_index];
+				x = x_range[0] + x_index * delta_x;
+				y = y_range[0] + y_index * delta_y;
 
-				matrix[x_index][y_index] = mandelbrot(z);
+				matrix[x_index][y_index] = {c: {r: x, i: y}, v: {r: 0, i: 0}, o: null};
 			}
 		}
-	}
 
-	canvas = document.getElementById("mandelbrot");
-	canvas.width = pixels;
-	canvas.height = pixels;
-	ctx = canvas.getContext("2d");
-	imageData = ctx.getImageData(0, 0, pixels, pixels);
-	var data = imageData.data;
+		/** iteration **/
+		for (i = 0; i < iterations; i++) {
+			for (var x_index = 0; x_index < pixels; x_index++) {
+				for (var y_index = 0; y_index < pixels; y_index++) {
+					z = matrix[x_index][y_index];
 
-	for (var x = 0; x < pixels; x++) {
-		for (var y = 0; y < pixels; y++) {
-			i = (y * pixels + x) * 4;
-			data[i + 3] = colormap(matrix[x][y]);
+					matrix[x_index][y_index] = mandelbrot(z, i);
+				}
+			}
 		}
+
+		canvas = document.getElementById("mandelbrot");
+		canvas.width = pixels;
+		canvas.height = pixels;
+		ctx = canvas.getContext("2d");
+		imageData = ctx.getImageData(0, 0, pixels, pixels);
+		var data = imageData.data;
+
+		for (var x = 0; x < pixels; x++) {
+			for (var y = 0; y < pixels; y++) {
+				i = (y * pixels + x) * 4;
+				data[i + 1] = 255 - colormap(matrix[x][y]);
+				data[i + 3] = 255;
+			}
+		}
+
+		ctx.putImageData(imageData, 0, 0);
+		console.log("done!");
 	}
 
-	ctx.putImageData(imageData, 0, 0);
-	console.log("done!")
+	full_mandelbrot();
+
+	canvas.addEventListener("click", function (event) {
+		var center, click;
+
+		click = {x: event.offsetX, y: event.offsetY};
+
+		range_x = x_range[1] - x_range[0];
+		range_y = y_range[1] - y_range[0];
+
+		center = {}
+		center["x"] = (x_range[0] + x_range[1]) / 2;
+		center["y"] = (y_range[0] + y_range[1]) / 2;
+
+		top_left = {x: center.x - range_x / 2, y: center.y - range_y / 2};
+
+		zoom_x = top_left.x + click.x / pixels * range_x;
+		zoom_y = top_left.y + click.y / pixels * range_y;
+		zoom = {x: zoom_x, y: zoom_y};
+
+		x_range = [zoom.x - range_x / 16, zoom.x + range_x / 16];
+		y_range = [zoom.y - range_y / 16, zoom.y + range_y / 16];
+
+		full_mandelbrot();
+	});
 
 })(this);
