1cee5c743da15ac8d84ab27586683893a2b87417
Jonathan Potter <jonathan.potter.business@gmail.com>
2015-08-29 18:04:57 -0700

    adds actual tests for parseLocationHash
    note: there is probably a library for this functionality, but w/e

--- test/toolsSpec.js
+++ test/toolsSpec.js
@@ -4,12 +4,43 @@ import { parseLocationHash } from 'javascript/tools';
 
 describe('Tools', () => {
   describe('parseLocationHash', () => {
-    it('parses location.hash into an object', () => {
-      /* this doesn't actually test doesn’t actually test anything… */
-      /* its just here to get the jasmine ball rolling */
+    it('returns an object', () => {
       let object = parseLocationHash();
 
       expect(typeof object).toEqual('object');
+    });
+
+    it('parses numbers numbers into numbers', () => {
+      let object = parseLocationHash('#number=1');
+
+      expect(typeof object.number).toEqual('number');
+    });
+
+    it('parses strings into strings', () => {
+      let object = parseLocationHash('#string=imastring');
+
+      expect(typeof object.string).toEqual('string');
+    });
+
+    it('parses float values', () => {
+      let object = parseLocationHash('#number=1.25');
+
+      expect(object.number).toEqual(1.25);
+    });
+
+    it('parses multiple values', () => {
+      let number = 'integer=1234';
+      let float  = 'float=1.234';
+      let string = 'string=rawr';
+
+      let locationHash = `#${number}&${float}&${string}`;
+      let object = parseLocationHash(locationHash);
+
+      expect(object).toEqual({
+        integer: 1234,
+        float: 1.234,
+        string: 'rawr'
+      });
     });
   });
 });
